46658ada18b47e977ec265164938fc259b5f03c9
mirluiz <zey.verdiyev@gmail.com>
2024-01-20 03:25:25 +0400

    save

--- src/__test__/polygon.test.ts
+++ src/__test__/polygon.test.ts
@@ -20,7 +20,7 @@ describe("Polygon", () => {
 
    */
 
-  test("Triangulation polygon with hole", () => {
+  test("Triangulation polygon with simple hole", () => {
     const ver0 = { val: "0", pos: { x: 0, y: 0 } };
     const ver1 = { val: "1", pos: { x: 4, y: 0 } };
     const ver2 = { val: "2", pos: { x: 4, y: 4 } };
@@ -42,6 +42,111 @@ describe("Polygon", () => {
     graph.addEdge(ver6, ver7);
     graph.addEdge(ver7, ver4);
 
-    let res = Polygon.connectInnerOuter(Object.values(graph.vertices), graph);
+    let outer = [ver0, ver1, ver2, ver3];
+    let inner = [ver4, ver5, ver6, ver7];
+    // let res = Polygon.connectInnerOuter(Object.values(graph.vertices), graph);
+    let res = Polygon.getVisiblePair(inner, outer, graph);
+    console.log("res", res);
+  });
+
+  test("Triangulation polygon with hole", () => {
+    const ver0 = { val: "0", pos: { x: 0, y: 0 } };
+    const ver1 = { val: "1", pos: { x: 4, y: 2 } };
+    const ver2 = { val: "2", pos: { x: 8, y: -1 } };
+    const ver3 = { val: "3", pos: { x: 12, y: -2 } };
+
+    const ver4 = { val: "4", pos: { x: 8, y: -4 } };
+    const ver5 = { val: "5", pos: { x: 6, y: -6 } };
+    const ver6 = { val: "6", pos: { x: 4, y: -6 } };
+    const ver7 = { val: "7", pos: { x: 4.5, y: -3 } };
+
+    const ver8 = { val: "8", pos: { x: 5, y: -5 } };
+    const ver9 = { val: "9", pos: { x: 6, y: -4.8 } };
+
+    const ver10 = { val: "10", pos: { x: 7.5, y: -2.5 } };
+    const ver11 = { val: "11", pos: { x: 4.5, y: -2 } };
+    const ver12 = { val: "12", pos: { x: 3.5, y: -6.5 } };
+    const ver13 = { val: "13", pos: { x: 3.5, y: -8.5 } };
+    const ver14 = { val: "14", pos: { x: 1.5, y: -6.5 } };
+
+    const ver15 = { val: "15", pos: { x: 1.5, y: -1.5 } };
+    const ver16 = { val: "16", pos: { x: 3.5, y: -3.6 } };
+    const ver17 = { val: "17", pos: { x: 2.5, y: 0.5 } };
+
+    const graph = new Graph();
+
+    let outer = [
+      ver0,
+      ver1,
+      ver2,
+      ver3,
+      ver4,
+      ver5,
+      ver6,
+      ver7,
+      ver8,
+      ver9,
+      ver10,
+      ver11,
+      ver12,
+      ver13,
+      ver14,
+    ];
+    let inner = [ver15, ver16, ver17];
+    // let res = Polygon.connectInnerOuter(Object.values(graph.vertices), graph);
+    let res = Polygon.getVisiblePair(inner, outer, graph);
+    console.log("res", res);
+  });
+
+  test("Intersection", () => {
+    let line1Ver = { x: 4, y: -3 };
+    let line2Ver = { x: 4, y: 3 };
+
+    let line = {
+      start: new THREE.Vector3(line1Ver.x, 0, line1Ver.y),
+      end: new THREE.Vector3(line2Ver.x, 0, line2Ver.y),
+    };
+    let origin = new THREE.Vector3(0, 0, 0);
+    let direction = new THREE.Vector3(1, 0, 0);
+
+    let res = Polygon.intersect({ origin, direction }, line);
+  });
+
+  test("Intersection case 1", () => {
+    const ver0 = { val: "0", pos: { x: 0, y: 0 } };
+    const ver1 = { val: "1", pos: { x: 4, y: 2 } };
+    const ver2 = { val: "2", pos: { x: 8, y: -1 } };
+    const ver3 = { val: "3", pos: { x: 12, y: -2 } };
+
+    const ver4 = { val: "4", pos: { x: 8, y: -4 } };
+    const ver5 = { val: "5", pos: { x: 6, y: -6 } };
+    const ver6 = { val: "6", pos: { x: 4, y: -6 } };
+    const ver7 = { val: "7", pos: { x: 4.5, y: -3 } };
+
+    const ver8 = { val: "8", pos: { x: 5, y: -5 } };
+    const ver9 = { val: "9", pos: { x: 6, y: -4.8 } };
+
+    const ver10 = { val: "10", pos: { x: 7.5, y: -2.5 } };
+    const ver11 = { val: "11", pos: { x: 4.5, y: -2 } };
+    const ver12 = { val: "12", pos: { x: 3.5, y: -6.5 } };
+    const ver13 = { val: "13", pos: { x: 3.5, y: -8.5 } };
+    const ver14 = { val: "14", pos: { x: 1.5, y: -6.5 } };
+
+    const ver15 = { val: "15", pos: { x: 1.5, y: -1.5 } };
+    const ver16 = { val: "16", pos: { x: 3.5, y: -3.6 } };
+    const ver17 = { val: "17", pos: { x: 2.5, y: 0.5 } };
+
+    let line1Ver = ver1.pos;
+    let line2Ver = ver2.pos;
+
+    let line = {
+      start: new THREE.Vector3(line1Ver.x, 0, line1Ver.y),
+      end: new THREE.Vector3(line2Ver.x, 0, line2Ver.y),
+    };
+    let origin = new THREE.Vector3(ver16.pos.x, 0, ver16.pos.y);
+    let direction = new THREE.Vector3(1, 0, 0);
+
+    let res = Polygon.intersect({ origin, direction }, line);
+    console.log("res", res);
   });
 });
--- src/app/system/utils/Polygon.ts
+++ src/app/system/utils/Polygon.ts
@@ -1,4 +1,3 @@
-import { Corner, Wall } from "../../model";
 import { Graph, Vertex } from "./Graph";
 import { Geometry } from "../interfaces";
 import Vector3 = Geometry.Vector3;
@@ -6,30 +5,39 @@ import Line = Geometry.Line;
 
 class Polygon {
   static connectInnerOuter(vertices: Vertex[], graph: Graph) {}
-  static getVisiblePair(inner: Corner[], outer: Corner[]) {
-    let pair: Corner | null = null;
+  static getVisiblePair(inner: Vertex[], outer: Vertex[], graph: Graph) {
+    let pair: Vertex | null = null;
 
-    let maxRight = [...inner].sort((a, b) => a.position.x - b.position.x)[0];
+    let maxRight = [...inner].sort((a, b) => b.pos.x - a.pos.x)[0];
 
-    let intersectedWall: Wall | undefined;
+    let intersectedWall:
+      | (Line & { connections: { start: Vertex; end: Vertex } })
+      | undefined;
 
-    outer.map((corner) => {
-      corner.walls.map((wall) => {
-        let origin = new Vector3(
-          maxRight.position.x,
-          maxRight.position.y,
-          maxRight.position.z
-        );
+    outer.map((vertex, index, array) => {
+      if (intersectedWall) return;
 
-        let intersect = this.intersect(
-          { origin, direction: new Vector3(1, 0, 0) },
-          wall
-        );
+      let nextVertex = array[(index + 1) % array.length];
+      let line = {
+        start: new Vector3(vertex.pos.x, 0, vertex.pos.y),
+        end: new Vector3(nextVertex.pos.x, 0, nextVertex.pos.y),
+        connections: { start: vertex, end: nextVertex },
+      };
+      let origin = new Vector3(maxRight.pos.x, 0, maxRight.pos.y);
 
-        if (intersect) {
-          intersectedWall = wall;
-        }
-      });
+      let intersect = this.intersect(
+        { origin, direction: new Vector3(1, 0, 0) },
+        line
+      );
+
+      if (intersect) {
+        console.log("line", line);
+        console.log("vertex", vertex);
+        console.log("nextVertex", nextVertex);
+        console.log("origin", origin);
+        console.log("maxRight", maxRight);
+        intersectedWall = line;
+      }
     });
 
     if (intersectedWall) {
@@ -38,23 +46,29 @@ class Polygon {
           ? intersectedWall.connections.start
           : intersectedWall.connections.end;
 
-      if (maxRightOnWall instanceof Corner) {
-        pair = maxRightOnWall;
-      }
+      pair = maxRightOnWall;
     }
 
-    return pair;
+    return {
+      maxRight,
+      pair,
+      intersectedWall: {
+        start: intersectedWall?.connections.start,
+        end: intersectedWall?.connections.end,
+      },
+    };
   }
 
-  private static intersect(
+  static intersect(
     ray: { origin: Vector3; direction: Vector3 },
     line: Line
   ): { position: Vector3; line: Line } | null {
     let { direction, origin } = ray;
     let lineDirection = line.end.clone().sub(line.start.clone()).normalize();
+    let lineLn = line.end.clone().sub(line.start.clone()).length();
 
     const crossProduct =
-      direction.x * lineDirection.z - direction.z * lineDirection.x;
+      lineDirection.x * direction.z - lineDirection.z * direction.x;
 
     // Check if the lines are parallel
     if (crossProduct === 0) {
@@ -66,8 +80,7 @@ class Polygon {
         (line.start.x - origin.x) * lineDirection.z) /
       crossProduct;
 
-    if (t >= 0) {
-      // Intersection point
+    if (t >= 0 && t <= lineLn) {
       return {
         position: new Vector3(
           ray.origin.x + t * ray.direction.x,
